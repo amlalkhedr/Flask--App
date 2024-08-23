ad31a06eb39ddbc52bb3b5bbb91e33f7ec1574dd
AhmedWael2397 <161589601+AhmedWael2397@users.noreply.github.com>
2024-08-23 21:30:13 +0300

    Full-process script modification

--- Full-Process.sh
+++ Full-Process.sh
@@ -38,6 +38,20 @@ terraform init -input=false
 #Apply the Terraform configuration and capture output , AUTO APPROVED 
 terraform apply -auto-approve
 
+#sync the aws eks to be accessed with kubectl 
+aws eks --region eu-central-1 update-kubeconfig --name Sprints-Eks
+
+# install nexus repo on cluster
+cd ../../K8s-Cluster-Files/nexus
+kubectl apply -f .
+
+#install helm charts on cluster
+cd .. 
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+helm install ingress-nginx ingress-nginx/ingress-nginx
+helm install sprints-app ./sprints-app
+
 #Check if the output was successfully retrieved
 if [ -z "$JENKINS_IP" ]; then
     echo "Failed to retrieve Jenkins ip from Terraform."
@@ -45,11 +59,8 @@ if [ -z "$JENKINS_IP" ]; then
 fi
 
 #Add the IP to the Ansible configuration file
-
-cd ../../ansible
 echo "Adding public IP to Ansible configuration file..."
 
-#If you want to add it to a specific section, like defaults or inventory
 echo "[EC2s]" > "$ANSIBLE_CONFIG_FILE"
 echo "Jenkins ansible_host=${JENKINS_IP} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa
 " >> "$ANSIBLE_CONFIG_FILE"
@@ -57,13 +68,13 @@ echo "Jenkins ansible_host=${JENKINS_IP} ansible_user=ubuntu ansible_ssh_private
 #Provide feedback to the user
 echo "Terraform public IP has been added to the Ansible configuration file: $ANSIBLE_CONFIG_FILE"
 
+#install jenkins on ec2 
+cd ../Ansible
 ansible-playbook -i inventory.ini Jenkins-install-playbook.yml -K
-
+ansible-playbook -i inventory.ini 
 
 echo "**************************************************"
 echo " NOW GO INSTALL JENKINS needed plugins Manually on GUI "
 echo "**************************************************"
-###### EMPTY ansible configuration file for future script usage ###########
-#truncate -s 0 $ANSIBLE_CONFIG_FILE
 
 #End of script
