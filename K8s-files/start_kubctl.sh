25176141f263075bb0e29704fdebfea509c6a1e6
Ahmed Mahmoud Ali <108240402+wwmezo0@users.noreply.github.com>
2024-10-18 20:57:12 +0300

    Update start_kubctl.sh

--- K8s-files/start_kubctl.sh
+++ K8s-files/start_kubctl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 aws eks --region us-east-1 update-kubeconfig --name my-eks-cluster 
-
+kubectl delete -f flask_app_deployment.yaml
 kubectl apply -f mysql_pv.yaml
 kubectl apply -f mysql_pvc.yaml
 kubectl apply -f mysql_deployment.yaml
